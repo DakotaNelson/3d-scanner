132d842333e8df6fe9400a7826c4139d4471fd92
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-09-23 12:28:29 -0400

    added semilcolon to arduino code, adapted python code to new origin and serial data type

--- _3Scanner_ino/_3Scanner_ino.ino
+++ _3Scanner_ino/_3Scanner_ino.ino
@@ -5,41 +5,44 @@
 
 #ifndef SERIAL_TIMEOUT
 #define SERIAL_TIMEOUT      5
-#endif
-0;
-int column = 64;
-
-
+#endif 
 String inString = "";    // string to hold input
+
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
 }
 
-void scan(resolution){
+void scan(int resolution){
   int panMotor = 3;
   int tiltMotor = 10;
   int sensor = A0;
+  int column = 64;
   
+  analogWrite(panMotor, 64);
+  analogWrite(tiltMotor, 64);
   while(column < 128){
     //scan direction 1
     for(int d1 = 64; d1<128; d1++){
       analogWrite(panMotor, d1); //scans one direction horizontally
-      analogRead(sensor);
+      //Serial.println(analogRead(sensor));
+      Serial.println(d1);
+      Serial.println(column);
+      delay(100);
     }
-    analogWrite(tiltMotor, column)//scans up one postion
-    column++;
+    column+=1;
+    analogWrite(tiltMotor, column);   //scans up one postion
     //scans direction 2
-    for(int d2 = 128; d2>64; d1--){
+    for(int d2 = 128; d2>64; d2--){
       analogWrite(panMotor, d2);
-      analogRead(sensor);
+      //Serial.println(analogRead(sensor));
+      Serial.println(d2);
+      Serial.println(column);
+      delay(100);
     }
-    analogWrite(tiltMotor, column);
-    column++;
+    column+=1;
+    analogWrite(tiltMotor, column);   
   }
         
 }
@@ -56,13 +59,9 @@ void loop() {
     // if you get a newline, print the string,
     // then the string's value:
     if (inChar == '\n') {
-      Serial.println(inString.toInt());
+      //Serial.println(inString.toInt());
     //resolution is the value sent over serial
-    scan(inString.toInt());
-        // clear the string for new input:
-      inString = ""; 
+  scan(inString.toInt());
     }
-  }
+ }
 }
-
-
--- scan.py
+++ scan.py
@@ -1,7 +1,7 @@
 """ Code to collect 3d scan data"""
 
-import pyplot.matplotlib as plt 
-import arduino as a
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 import serial
 import sys
 import time
@@ -18,44 +18,46 @@ def capture_data(resolution, arduino = serial.Serial("/dev/ttyACM0", 115200)):
         coordinates -> unprocessed list of points 
     """
     #tells arduino the step size
-    arduino.write(resolution)
+    arduino.write(str(resolution))
     coordinates = []
 
     #sets the number of expected points (so the loop will know when to exit)
-    totalPoints = (64/resolution)**2
+    totalPoints = (128*3/resolution)**2
 
-    while len(coordinates < totalPoints):
-        #While we don't have all the data yet,
-        #read it from the serial and append to a list
-        data = arduino.readline().rstrip('\n') 
+    while len(coordinates) < 10: #totalPoints):
+        #read data from the serial 
+        data = arduino.readline().rstrip('\n')
         coordinates.append(data)
     return coordinates
 
-def processData(rawData):
+def processData(raw_data):
     """
     Does math to turn sensor and servo values into physical distances
 
     Inputs:
-        rawData -> list of 3 value coordinate pairs
+        rawData -> list of coordinate values, every three make one 
+                   coordinate set 
     Outputs:
         processedData -> list of real world coordinates in units
     """
-    x = []
-    y =  []
-    z = []
     #Assuming origin is at motor when at center of pan and tilt
 
     callibration = 0 #Fill in with acutal callibration value
-    for coordinate in rawData:
-        distance = coordinate[2] * callibration
-        z.append(distance)
+    for index in range(len(raw_data)):
+        depth = raw_data[index] * callibration
 
         #deals with conversion between servo value and angle
         angle_conversion = (np.py/4)/128
+
         #converts to cartesian shifting origin to center servo value
-        horizontal_coordinate = distance*np.cos((64-x)*angle_conversion)
+        horizontal_coordinate = depth*np.cos((96-raw_data[1+index])*angle_conversion)
+
         #converts to cartesian leaving origin at base for y
-        vertical_coordinate = distance*np.sin((y)*angle_conversion)
+        vertical_coordinate = depth*np.sin(96 -(raw_data[2+index])*angle_conversion)
+        x.append(horizontal_coordinate)
+        y.append(vertical_coordinate)
+        z.append(depth)
+    return x,y,z
 
     
 
@@ -65,5 +67,10 @@ def processData(rawData):
 #function to rotate 1 row
 
 if __name__ == '__main__':
-    s = Scanner(3)
-    s.tilt(100)
+    raw = capture_data(1)
+    print raw 
+    x,y,z = processedData(raw)
+    print x
+    # fig = plt.figure()
+    # Axes3D.scatter(X,Y, Z)
+    # plt.show()
