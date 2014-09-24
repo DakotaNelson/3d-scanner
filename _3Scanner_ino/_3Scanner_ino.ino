713d516801eb8b93a7601f1b5b695d201ae9a3f3
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-09-24 17:08:02 -0400

    pan/tilt mechanism now scans

--- _3Scanner_ino/_3Scanner_ino.ino
+++ _3Scanner_ino/_3Scanner_ino.ino
@@ -1,67 +1,95 @@
+#include <Servo.h>
 
-#ifndef SERIAL_RATE
-#define SERIAL_RATE         115200
-#endif
+Servo pan_servo;
+Servo tilt_servo;
 
-#ifndef SERIAL_TIMEOUT
-#define SERIAL_TIMEOUT      5
-#endif 
+const int SERIAL_RATE = 115200;
+const int STEP = 1; // move one degree between scans
+
+const int PAN_PIN = 3;
+const int TILT_PIN = 10;
+
+
 String inString = "";    // string to hold input
-
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(SERIAL_RATE);
+  pan_servo.attach(PAN_PIN);
+  tilt_servo.attach(TILT_PIN);
 }
 
 void scan(int resolution){
-  int panMotor = 3;
-  int tiltMotor = 10;
   int sensor = A0;
-  int column = 64;
+  int column = 45; // current state of the pan servo
   
-  analogWrite(panMotor, 64);
-  analogWrite(tiltMotor, 64);
-  while(column < 128){
+  pan(column);
+  tilt(90); // set initial position of servos
+  
+  // do all the scanning
+  while(column < 135) {
+    
     //scan direction 1
-    for(int d1 = 64; d1<128; d1++){
-      analogWrite(panMotor, d1); //scans one direction horizontally
+    for(float d1 = 90; d1 < 135; d1 += STEP){
+      tilt(d1); //scans one direction horizontally
       //Serial.println(analogRead(sensor));
       Serial.println(d1);
       Serial.println(column);
       delay(100);
     }
-    column+=1;
-    analogWrite(tiltMotor, column);   //scans up one postion
-    //scans direction 2
-    for(int d2 = 128; d2>64; d2--){
-      analogWrite(panMotor, d2);
+    column += STEP;
+    pan(column);   //move over one step
+    delay(100);
+    
+    //scans in the other direction
+    for(int d2 = 135; d2 > 90; d2 -= STEP){ 
+      tilt(d2);
       //Serial.println(analogRead(sensor));
       Serial.println(d2);
       Serial.println(column);
       delay(100);
     }
-    column+=1;
-    analogWrite(tiltMotor, column);   
-  }
-        
+    column += STEP;
+    pan(column);
+    delay(100);   
+  } // end scanning loop
 }
 
 void loop() {
   // Read serial input:
-  while (Serial.available() > 0) {
-    int inChar = Serial.read();
-    if (isDigit(inChar)) {
-      // convert the incoming byte to a char 
-      // and add it to the string:
-      inString += (char)inChar; 
-    }
-    // if you get a newline, print the string,
-    // then the string's value:
-    if (inChar == '\n') {
-      //Serial.println(inString.toInt());
-    //resolution is the value sent over serial
-  scan(inString.toInt());
-    }
- }
+//  while (Serial.available() > 0) {
+//    int inChar = Serial.read();
+//    if (isDigit(inChar)) {
+//      // convert the incoming byte to a char 
+//      // and add it to the string:
+//      inString += (char)inChar; 
+//    }
+//    // if you get a newline, print the string,
+//    // then the string's value:
+//    if (inChar == '\n') {
+//      //Serial.println(inString.toInt());
+//    //resolution is the value sent over serial
+//      scan(inString.toInt());
+//    }
+// }
+  scan(10);
+}
+
+bool pan(int deg) {
+  // deg as in degrees
+  // Pan to the correct angle - include calibration
+  
+  pan_servo.write(deg);
+  //TODO include calibration
+
+  return true; // if successful
+}
+
+bool tilt(int deg) {
+  // Tilt to the correct angle - include calibration
+  
+  tilt_servo.write(deg);
+  //TODO include calibration
+  
+  return true; // if successful
 }
